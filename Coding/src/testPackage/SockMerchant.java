6badd4ef6eb907f2898cc19307f7e7255bf303ef
ashutoshksingh <ashutoshksingh@USHYDASHUTOSH2.us.deloitte.com>
2019-09-27 16:17:25 +0530

    new program ElectronicShop

--- Coding/src/testPackage/SockMerchant.java
+++ Coding/src/testPackage/SockMerchant.java
@@ -8,11 +8,11 @@ import java.util.Map;
 public class SockMerchant {
 
 	public void pairFinder() {
-		
-		//this comment added from git hub 
+
+		// this comment added from git hub
 
 		int pairs = 0;
-		List<Integer> lst = Arrays.asList(1 ,1 ,3 ,1 ,2 ,1 ,3 ,3 ,3 ,3 );
+		List<Integer> lst = Arrays.asList(1, 1, 3, 1, 2, 2, 3, 3, 3, 2);
 		Map<Integer, Integer> m = new HashMap<Integer, Integer>();
 		/*
 		 * lst.stream().forEach(i -> { if (m.containsKey(i)) m.put(i, m.get(i) + 1);
@@ -28,13 +28,13 @@ public class SockMerchant {
 
 		System.out.println("Status of the Map " + m);
 
-		/*for (int i : m.keySet()) {
-			pairs = pairs + m.get(i) / 2;
-		}*/
+		/*
+		 * for (int i : m.keySet()) { pairs = pairs + m.get(i) / 2; }
+		 */
 
 		for (int i : m.values())
 			pairs = pairs + i / 2;
-			
+
 		System.out.println("No. of pairs in the array: " + pairs);
 	}
 
