ca8fefb77dbc5dbba1ace89816fb326f1ad63815
ashutoshksingh <ashutoshksingh@USHYDASHUTOSHK2.us.deloitte.com>
2020-02-18 23:32:08 +0530

    2 d matrix to print n prime number

--- Coding/src/testPackage/Eullerproblem1.java
+++ Coding/src/testPackage/Eullerproblem1.java
@@ -8,31 +8,31 @@ public class Eullerproblem1 {
 	// Find the sum of all the multiples of 3 or 5 below 1000.
 
 	static int sum = 0;
-	static int i=1 ;
-	static List<Integer> lt = new ArrayList<Integer>();
+	static int i = 1;
+	static int size = 0;
 
-	public static void method1(int n1, int n2,int limit)
+	public static void method1(int n1, int n2, int limit)
 
 	{
-		
-		while (lt.size() < limit) {
+
+		while (size < limit) {
 
 			if (i % n1 == 0 || i % n2 == 0) {
 				sum = sum + i;
 
 				System.out.println(i);
-				lt.add(i);
-				
+				// lt.add(i);
+				size++;
+
 			}
 			i++;
-			
 
 		}
 
-		System.out.println("<<>>>" + sum + "<<>>" + lt.size());
+		System.out.println("<<>>>" + sum + "<<>>");
 	}
 
 	public static void main(String args[]) {
-		method1(3, 5,1000);
+		method1(14,7,20);
 	}
 }
