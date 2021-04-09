2680a140bf818a42f657354cc27a48d704908bd1
ashutosh0810 <39034846+ashutosh0810@users.noreply.github.com>
2021-04-09 23:57:38 +0530

    push

---
+++ Coding/Codingsrc/practiceonly/LamExperssion.java
@@ -0,0 +1,51 @@
+package practiceonly;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LamExperssion {
+	HashMap<Character, Integer> hash = new HashMap<>();
+
+	void freqchar(String s) {
+		char ch[] = s.replaceAll("\\s+", "").toCharArray();
+
+		if (ch.length == 0) {
+			System.out.println(" string is empty ");
+		}
+	
+		else {
+			for (char eachCharacter : ch) {
+				if (hash.containsKey(eachCharacter)) {
+					hash.put(eachCharacter, hash.get(eachCharacter) + 1);
+				} else {
+					hash.put(eachCharacter, 1);
+				}
+			}
+			System.out.println(" map  " + hash);
+
+			int maxcount = 0;
+			char maxChar = 0;
+			for (Map.Entry<Character, Integer> entry : hash.entrySet()) {
+				if (entry.getValue() > maxcount) {
+					maxcount = entry.getValue();
+					maxChar = entry.getKey();
+
+				}
+				System.out.println("set" + entry);
+			}
+	
+			System.out.println(maxChar + " frequency  " + maxcount);
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Integer> l = Arrays.asList(1, 2, 4, 5, 6);
+		int a[] = { 3, 6, 8, 9, 21 };
+		LamExperssion obj = new LamExperssion();
+		obj.freqchar("my name is ashutosh");
+	}
+
+}
