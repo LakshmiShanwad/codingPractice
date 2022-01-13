17d187d950b6b6a3c7a4b80b856b033bf4cc6648
ashutosh0810 <njoyashu@gmail.com>
2022-01-14 00:14:56 +0530

    just test in intellij

--- Coding/Codingsrc/practiceonly/Ds.java
+++ Coding/Codingsrc/practiceonly/Ds.java
@@ -1,6 +1,20 @@
 package practiceonly;
 
+import java.util.Arrays;
+
 public class Ds extends Throwable{
+
+    public  static void main(String args[])
+    {
+        int[] array={2,5,3,6};
+        Arrays.sort(array);
+        for(int i =0 ; i<array.length;i++)
+        {
+            System.out.println(array[i]);
+        }
+
+    }
+
 
 	
 	
