9ee8d986f99786ab59d0bd6adf08301336198241
ashutosh0810 <njoyashu@gmail.com>
2022-01-14 00:45:27 +0530

    changing test data

--- Coding/Codingsrc/codingpackage/QuickSort.java
+++ Coding/Codingsrc/codingpackage/QuickSort.java
@@ -3,7 +3,7 @@ package codingpackage;
 public class QuickSort {
 	// ascendign order  by divide and conquer rule
 	//choose a pivot point and compare with this pivot point and swap
-	//test comment
+
 
 	int partition(int a[], int low, int high) {
 		int p = a[(low + high) / 2];
