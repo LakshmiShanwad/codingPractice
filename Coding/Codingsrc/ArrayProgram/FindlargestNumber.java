ff8413ca218530f1e8aab62ad953854d71a2aff4
ashutosh0810 <njoyashu@gmail.com>
2022-01-20 23:50:22 +0530

    New String code , to form a large number in string form

--- Coding/Codingsrc/ArrayProgram/FindlargestNumber.java
+++ Coding/Codingsrc/ArrayProgram/FindlargestNumber.java
@@ -4,89 +4,71 @@ public class FindlargestNumber {
     // Find largest number without using sorting
 
 
-    static int findLargest(int[] a)
-    {
-        if(a.length==1)
-        {
-            return a[a.length-1];
+    static int findLargest(int[] a) {
+        if (a.length == 1) {
+            return a[a.length - 1];
 
         }
         int n = Integer.MIN_VALUE;
-        for(int i=0;i<a.length;i++)
-        {
-            if(a[i]>n)
-            {
-                n=a[i];
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] > n) {
+                n = a[i];
             }
         }
         return n;
     }
 
     // Find Second largest number without using sorting
-    static int secondLargest(int a[])
-    {
-        int first=Integer.MIN_VALUE;
-        int second =Integer.MIN_VALUE;
+    static int secondLargest(int a[]) {
+        int first = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
 
-        if(a.length<2)
-        {
-            return  -1;
+        if (a.length < 2) {
+            return -1;
         }
-        for(int i=0;i<a.length;i++)
-        {
-            if(a[i]>first)
-            {
-                second=first;
-                first=a[i];
-            }
-            else if(a[i]>second)
-            {
-                second=a[i];
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] > first) {
+                second = first;
+                first = a[i];
+            } else if (a[i] > second) {
+                second = a[i];
             }
         }
-        return  second;
+        return second;
 
     }
 
     // find largest 3 number in an array without using sort;
 
-    static int threeLargest(int a[])
-    {
-        int first=Integer.MIN_VALUE;
-        int second =Integer.MIN_VALUE;
+    static int threeLargest(int a[]) {
+        int first = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
         int third = Integer.MIN_VALUE;
 
-        if(a.length<3)
-        {
-            return  -1;
+        if (a.length < 3) {
+            return -1;
         }
-        for(int i=0;i<a.length;i++)
-        {
-            if(a[i]>first)
-            {
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] > first) {
+                // this sequence is fix
                 third = second;
-                second=first;
-                first=a[i];
-            }
-            else if(a[i]>second)
-            {
-                third=second;
-                second=a[i];
-            }
-            else if(a[i]>third)
-            {
-                third=a[i];
+                second = first;
+                first = a[i];
+            } else if (a[i] > second) {
+                third = second;
+                second = a[i];
+            } else if (a[i] > third) {
+                third = a[i];
             }
         }
-        return  second*first*third;
+        return second * first * third;
 
     }
 
-    public  static  void  main(String args[])
-    {
-        System.out.println(findLargest(new int[]{1000,1258,12,1,2,9,5,23}));
-        System.out.println(secondLargest(new int[]{100,25485,-987455455}));
-        System.out.println(threeLargest(new int[]{-8,10,1,2}));
+    public static void main(String args[]) {
+        System.out.println(findLargest(new int[]{1000, 1258, 12, 1, 2, 9, 5, 23}));
+        System.out.println(secondLargest(new int[]{100, 25485, -987455455}));
+        System.out.println(threeLargest(new int[]{-8, 10, 1, 2}));
 
     }
 }
---
+++ Coding/Codingsrc/stringCoding/FormLargestNum.java
@@ -0,0 +1,72 @@
+package stringCoding;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class FormLargestNum {
+
+    //Find the largest number that can be formed in an integer array.
+    //you can convert it to a string in order to avoid the length issue
+
+    public static String formLargestNum(int[] num)
+    {
+        String[]str  = new String[num.length];
+        for (int i=0;i<num.length;i++)
+        {
+            str[i]=Integer.toString(num[i]);
+        }
+        Arrays.sort(str,(a,b)->{
+            long n1 = Long.parseLong(a+b);
+            long n2 = Long.parseLong(b+a);
+            if(n1>n2){
+                return 1;
+            }
+            else if(n1<n2)
+            {
+                return -1;
+            }
+            else {
+                return 0;
+            }
+        });
+        StringBuilder sb = new StringBuilder();
+        for(int i=str.length-1;i>=0;i--)
+        {
+            sb.append(str[i]);
+        }
+
+        return sb.toString();
+    }
+
+    public static String secondApproach(int[] num)
+    {
+        String str[] = new String[num.length];
+        for (int i=0;i<num.length;i++)
+        {
+            str[i]= Integer.toString(num[i]);
+        }
+        Arrays.sort(str, new Comparator<String>() {
+            @Override
+            public int compare(String a, String b) {
+                String o1=a+b;
+                String o2 =b+a;
+                return o2.compareTo(o1);
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for (String s : str)
+        {
+            sb.append(s);
+        }
+        return  sb.toString();
+    }
+    public static void main(String args[])
+    {
+
+        int nums[] ={22,3,31};
+        System.out.println(formLargestNum(nums));
+        System.out.println(secondApproach(nums));
+
+    }
+}
