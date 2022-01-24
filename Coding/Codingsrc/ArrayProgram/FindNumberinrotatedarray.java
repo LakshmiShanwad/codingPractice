a69fd29cb779edd674f5b41910024b778df61f61
ashutosh0810 <njoyashu@gmail.com>
2022-01-24 23:40:42 +0530

    new codes on binary search

--- Coding/Codingsrc/ArrayProgram/FindNumberinrotatedarray.java
+++ Coding/Codingsrc/ArrayProgram/FindNumberinrotatedarray.java
@@ -1,4 +1,54 @@
 package ArrayProgram;
 
 public class FindNumberinrotatedarray {
+
+/*    search the number's index in an circular array'
+* {4,5,6,7,1,2};
+* */
+
+    public static  int searchNumber(int[] arr,int target)
+    {
+        int low = 0;
+        int high= arr.length-1;
+        while (low<=high)  //{4,5,6,7,1,2};
+        {
+            int mid = low + (high-low)/2;
+            if(arr[mid]==target)
+            {
+                return mid;
+            }
+            else if(arr[low]<arr[mid])
+            {
+                if(target<arr[mid] && target>=arr[low])
+                {
+                    high=mid-1;
+                }
+                else
+                {
+                    low=mid+1;
+                }
+            }
+            else if(arr[mid]<arr[high])
+            {
+                if(target>arr[mid] && target< arr[high])
+                {
+                    low =mid+1;
+                }
+                else
+                {
+                    high=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String args[])
+    {
+        int array[] ={2,5,6,0,0,1,2};
+        int tar= 0;
+
+        System.out.println(searchNumber(array,tar));
+
+    }
 }
---
+++ Coding/Codingsrc/ArrayProgram/FindelementinrotatedSortedArray.java
@@ -0,0 +1,50 @@
+package ArrayProgram;
+
+public class FindelementinrotatedSortedArray {
+
+    //leet code 33
+
+    public static int search(int nums[], int target)
+    {
+        int low =0;
+        int high = nums.length-1;
+        while (low<=high)
+        {
+            int mid = low+(high-low)/2;
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+            else if(nums[low]<nums[mid])
+            {
+                if(target>nums[low] && target< nums[mid])
+                {
+                    high = mid-1;
+                }
+                else
+                {
+                    low =mid+1;
+                }
+            }
+            else if(nums[mid] < nums[high])
+            {
+                if(target>nums[mid] && target<=nums[high])
+                {
+                    low = mid+1;
+                }
+                else
+                {
+                  high=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String args[])
+    {
+        System.out.println(search(new int[]{4,5,6,1,2}, 2));
+
+        System.out.println(search(new int[]{1,2,8,9,10}, 8));
+    }
+}
