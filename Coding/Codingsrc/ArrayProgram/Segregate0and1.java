a20fa750890e07e74d01e7bd90ce0a091c331057
ashutosh0810 <njoyashu@gmail.com>
2022-10-08 00:04:46 +0530

    changing code segregate 0 1 added login for 11000

--- Coding/Codingsrc/ArrayProgram/Segregate0and1.java
+++ Coding/Codingsrc/ArrayProgram/Segregate0and1.java
@@ -28,13 +28,44 @@ public class Segregate0and1 {
         }
     }
 
+    public static  void  segregate10(int arr[])
+    {
+        int left = 0;
+        int right = arr.length-1;
+        while(left<right)
+        {
+            while (arr[left]==1 && left<right)
+            {
+                left++;
+            }
+            while (arr[right]==0 && left<right)
+            {
+                right--;
+            }
+            if(left<right)
+            {
+                arr[left]=1;
+                arr[right]=0;
+                left++;
+                right--;
+            }
+        }
+    }
+
 
     public static void main(String[] args) {
-        int[] input = {0,1,1,1,0,0,1,1,0,2};
+        int[] input = {0,1,1,1,0,0,1,1,0,1};
         segregate(input);
         for (int i=0;i<input.length;i++)
         {
-            System.out.print( input[i]+ ",");
+            System.out.print( input[i]+ "  ");
+        }
+        System.out.println("\n");
+
+        segregate10(input);
+        for (int i=0 ; i<input.length;i++)
+        {
+            System.out.print(input[i] + "  ");
         }
     }
 }
--- Coding/Codingsrc/codingpackage/BracketBalance.java
+++ Coding/Codingsrc/codingpackage/BracketBalance.java
@@ -4,47 +4,27 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 
 public class BracketBalance {
-
     // this is to balance the bracket
     // return true if input is balanced else return false .
 
-    public static boolean braceBalance(String str)
-
-
-    {
+    public static boolean braceBalance(String str) {
         Deque<Character> deque = new ArrayDeque<>();
-        for (Character ch: str.toCharArray()
-             ) {
-            if(ch=='(' || ch == '{' || ch == '[')
-            {
+        for (Character ch : str.toCharArray()
+        ) {
+            if (ch == '(' || ch == '{' || ch == '[') {
                 deque.push(ch);
-            }
-            else if (ch==')' && !deque.isEmpty() && deque.peek()=='(')
-            {
+            } else if (ch == ')' && !deque.isEmpty() && deque.peek() == '(') {
                 deque.pop();
-
-            }
-            else if (ch=='}' && !deque.isEmpty() && deque.peek()=='{')
-            {
+            } else if (ch == '}' && !deque.isEmpty() && deque.peek() == '{') {
                 deque.pop();
-
-            }
-            else if (ch==']' && !deque.isEmpty() && deque.peek()=='[')
-            {
+            } else if (ch == ']' && !deque.isEmpty() && deque.peek() == '[') {
                 deque.pop();
-
-            }
-            else
-            {
+            } else {
                 return false;
             }
-
-
         }
         return deque.isEmpty();
-
     }
-
 
     public static void main(String[] args) {
         System.out.println(braceBalance("(())"));
@@ -53,8 +33,5 @@ public class BracketBalance {
         System.out.println(braceBalance("(()"));
         System.out.println(braceBalance("))"));
         System.out.println(braceBalance("(("));
-
-
-
     }
 }
