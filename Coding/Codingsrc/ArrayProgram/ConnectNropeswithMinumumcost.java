34da6e23aa2219b7659afead491e95d6e82a3af5
ashutosh0810 <njoyashu@gmail.com>
2022-10-14 00:46:13 +0530

    ConnectNropeswithMinumumcost

---
+++ Coding/Codingsrc/ArrayProgram/ConnectNropeswithMinumumcost.java
@@ -0,0 +1,36 @@
+package ArrayProgram;
+
+import java.util.PriorityQueue;
+
+/*
+You have N ropes you have to connect those ropes to such that cost of connection is minimum
+assuming cost = length of the rope
+input = 2,5,4,8,6,9
+output = should be the minimum cost
+solution : Start with adding the small small rope
+
+ */
+public class ConnectNropeswithMinumumcost {
+    public static void main(String[] args) {
+        System.out.println(solve(new int[]{2,5,4,8,6,9}));
+    }
+
+    public static int solve(int arr[])
+    {
+        int answer=0;
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+
+        for (int i=0;i<arr.length;i++)
+        {
+            priorityQueue.add(arr[i]);
+        }
+        while (priorityQueue.size()>1) {
+            int first = priorityQueue.poll();
+            int second = priorityQueue.poll();
+            int sum =first + second;
+            answer+=sum;
+            priorityQueue.add(sum);
+        }
+        return answer;
+    }
+}
--- Coding/Codingsrc/stringCoding/ReverseStringwithouttreplacingspecialchar.java
+++ Coding/Codingsrc/stringCoding/ReverseStringwithouttreplacingspecialchar.java
@@ -1,35 +1,31 @@
 package stringCoding;
 
-public class ReverseStringwithouttreplacingspecialchar
-
-{
+public class ReverseStringwithouttreplacingspecialchar {
     public static void main(String[] args) {
-        solve("ab=cd+ef");
+        System.out.println(solve("ab=cd+ef"));
+        System.out.println(solve("asjkag**=cdcagbk//+ef"));
+        System.out.println(solve("**//*++*"));
+        System.out.println(solve("abcgd"));
     }
 
-    public static void solve(String str)
-    {
-      StringBuilder stringBuilder = new StringBuilder(str);
-      String[] s = new String[str.length()];
-      int left= 0;
-      int right = str.length()-1;
-      while (left<right)
-      {
-          if (!(Character.isLetter(str.charAt(left))))
-          {
-              left++;
-          }
-          else if (!Character.isLetter(str.charAt(right)))
-          {
-              right--;
-          }
-          else {
-              stringBuilder.setCharAt(left,str.charAt(right));
-              stringBuilder.setCharAt(right,str.charAt(left));
-              left++;
-              right--;
-          }
-      }
-        System.out.println(stringBuilder.toString());
+    public static String solve(String str) {
+        StringBuilder stringBuilder = new StringBuilder(str);
+        String[] s = new String[str.length()];
+        int left = 0;
+        int right = str.length() - 1;
+        while (left < right) {
+            if (!(Character.isLetter(str.charAt(left)))) {
+                left++;
+            } else if (!Character.isLetter(str.charAt(right))) {
+                right--;
+            } else {
+                stringBuilder.setCharAt(left, str.charAt(right));
+                stringBuilder.setCharAt(right, str.charAt(left));
+                left++;
+                right--;
+            }
+        }
+        // System.out.println(stringBuilder.toString());
+        return stringBuilder.toString();
     }
 }
