582894a679324ef52f1ef8afe5381dbf6e6229b5
ashutosh0810 <njoyashu@gmail.com>
2022-10-13 12:36:42 +0530

    Find the largest sub array that will contain equal number of 1 and 0

---
+++ Coding/Codingsrc/ArrayProgram/Findlargestsubarraywithequal1and0.java
@@ -0,0 +1,17 @@
+package ArrayProgram;
+
+/*
+Find the largest sub array that will contain equal number of 1 and 0
+input = {1,1, 0, 0, 1, 1, 0,  0 }
+replace the 0 with -1 and then
+find the largest subarray with sum 0 that will solve the issue
+ */
+public class Findlargestsubarraywithequal1and0 {
+    public static void main(String[] args) {
+        solve(new int[]{1,1, 0, 0, 1, 1, 0, 0}, 0);
+    }
+    public static void solve(int[] input , int targetsum)
+    {
+
+    }
+}
